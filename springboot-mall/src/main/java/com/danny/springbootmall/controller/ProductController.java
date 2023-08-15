0ebdf3a1274969e51ab211805dcbc3e7490e2e14
danny900221 <83628853+danny900221@users.noreply.github.com>
2023-08-15 16:39:16 +0800

    商品功能 - 實作修改商品

--- springboot-mall/src/main/java/com/danny/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/danny/springbootmall/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.naming.InsufficientResourcesException;
 import javax.validation.Valid;
 
 @RestController
@@ -32,6 +33,24 @@ public class ProductController {
         Product product=productService.getProductById(productId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+        Product product=productService.getProductById(productId);
+
+        if(product==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+
+        productService.updateProduct(productId,productRequest);
+
+        Product updatedProduct=productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+
     }
 
 }
