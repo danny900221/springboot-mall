2d337211028447c1428b3c874e393298e04a1b5f
danny900221 <83628853+danny900221@users.noreply.github.com>
2023-08-23 14:48:15 +0800

    訂單功能 - 檢查 user 是否存在、扣除商品庫存

--- springboot-mall/src/main/java/com/danny/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/danny/springbootmall/dao/impl/ProductDaoImpl.java
@@ -152,4 +152,16 @@ public class ProductDaoImpl implements ProductDao {
         }
         return sql;
     }
+
+    @Override
+    public void updateStock(Integer productId, Integer stock) {
+        String sql="UPDATE product SET stock =:stock,last_modified_date =:lastModifiedDate" +
+                " WHERE product_Id =:productId";
+        Map<String,Object> map=new HashMap<>();
+        map.put("productId",productId);
+        map.put("stock",stock);
+        map.put("lastModifiedDate",new Date());
+
+        namedParameterJdbcTemplate.update(sql,map);
+    }
 }
--- springboot-mall/src/main/java/com/danny/springbootmall/service/Impl/OrderServiceImpl.java
+++ springboot-mall/src/main/java/com/danny/springbootmall/service/Impl/OrderServiceImpl.java
@@ -1,34 +1,66 @@
 package com.danny.springbootmall.service.Impl;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.danny.springbootmall.dao.OrderDao;
 import com.danny.springbootmall.dao.ProductDao;
+import com.danny.springbootmall.dao.UserDao;
 import com.danny.springbootmall.dto.BuyItem;
 import com.danny.springbootmall.dto.CreateOrderRequest;
 import com.danny.springbootmall.model.Order;
 import com.danny.springbootmall.model.OrderItem;
 import com.danny.springbootmall.model.Product;
+import com.danny.springbootmall.model.User;
 import com.danny.springbootmall.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class OrderServiceImpl implements OrderService {
+    private final static Logger log= LoggerFactory.getLogger(OrderServiceImpl.class);
     @Autowired
     private OrderDao orderDao;
     @Autowired
     private ProductDao productDao;
+    @Autowired
+    private UserDao userDao;
     @Transactional
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+
+        //檢查 user 是否存在
+        User user=userDao.getUserById(userId);
+
+        if(user==null){
+            log.warn("該 userId {} 不存在", userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+
         int totalAmount=0;
         List<OrderItem> orderItemList=new ArrayList<>();
 
         for(BuyItem buyItem:createOrderRequest.getBuyItemList()){
             Product product=productDao.getProductById(buyItem.getProductId());
+
+            //檢查 product 是否存在，庫存是否足夠
+            if(product ==null){
+                log.warn(" 商品 {} 不存在",buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+            }else if(product.getStock()<buyItem.getQuantity()){
+                log.warn("商品 {} 的庫存不足，無法購買，剩餘庫存 {}，欲購買數量 {}",
+                        buyItem.getProductId(),product.getStock(),buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            productDao.updateStock(product.getProductId(),product.getStock()-buyItem.getQuantity());
+
 
             //計算總價錢
             int amount =buyItem.getQuantity()*product.getPrice();
@@ -43,9 +75,11 @@ public class OrderServiceImpl implements OrderService {
             orderItemList.add(orderItem);
         }
 
-       Integer orderId= orderDao.createOrder(userId,totalAmount);
-       orderDao.createOrderItems(orderId,orderItemList);
-       return orderId;
+        Integer orderId= orderDao.createOrder(userId,totalAmount);
+
+        orderDao.createOrderItems(orderId,orderItemList);
+
+        return orderId;
     }
 
     @Override
